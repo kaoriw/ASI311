4eb35a02f72f397a5652bca8d39bd2defad523e3
Kaori <kaori.watanabe@ensta-paristech.fr>
2018-10-12 14:46:30 +0200

    bonus 2

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -1,5 +1,6 @@
 package com.excilys.formation.battleships.android.ui;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +17,18 @@ public class PlayerNameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_name);
         mNameEditText = (EditText) findViewById(R.id.text_name);
+
     }
 
     public void onClickButton(View v) {
         String name = mNameEditText.getText().toString();
         if (!name.isEmpty()) {
             Toast.makeText(PlayerNameActivity.this, name, Toast.LENGTH_LONG).show();
-            BattleShipsApplication.getGame().init(name);
+            SharedPreferences preferences = getApplicationContext().getSharedPreferences("Pref", MODE_PRIVATE);
+            preferences.edit().putString("PlayerName", name).apply();
+            if(preferences.getString("Playername", name) != null) {
+                BattleShipsApplication.getGame().init(name);
+            }
         }
         else Toast.makeText(PlayerNameActivity.this, "Entrez votre nom", Toast.LENGTH_LONG).show();
     }
