e625779b0dde28e84e23035d4918a36dcc40f33f
Kaori <kaori.watanabe@ensta-paristech.fr>
2018-10-19 12:38:25 +0200

    bonus 2

--- app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
+++ app/src/main/java/com/excilys/formation/battleships/android/ui/PlayerNameActivity.java
@@ -1,8 +1,11 @@
 package com.excilys.formation.battleships.android.ui;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,24 +14,31 @@ import battleships.formation.excilys.com.battleships.R;
 
 public class PlayerNameActivity extends AppCompatActivity {
     EditText mNameEditText;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player_name);
-        mNameEditText = (EditText) findViewById(R.id.text_name);
-
+        preferences = getApplicationContext().getSharedPreferences("Pref", MODE_PRIVATE);
+        String savedName = preferences.getString("PlayerName", ""); //Voir si un nom est stocké dans Playername
+        if(!savedName.equals("")) { //si le nom existe déjà
+            BattleShipsApplication.getGame().init(savedName);  //commencer directement le jeu
+        }
+        else {
+            setContentView(R.layout.activity_player_name);
+            mNameEditText = (EditText) findViewById(R.id.text_name);
+            String name = mNameEditText.getText().toString();
+        }
     }
 
     public void onClickButton(View v) {
         String name = mNameEditText.getText().toString();
         if (!name.isEmpty()) {
             Toast.makeText(PlayerNameActivity.this, name, Toast.LENGTH_LONG).show();
-            SharedPreferences preferences = getApplicationContext().getSharedPreferences("Pref", MODE_PRIVATE);
             preferences.edit().putString("PlayerName", name).apply();
-            if(preferences.getString("Playername", name) != null) {
-                BattleShipsApplication.getGame().init(name);
-            }
+            Log.d("PlayerName", preferences.getString("PlayeName", name));
+            BattleShipsApplication.getGame().init(name);
+
         }
         else Toast.makeText(PlayerNameActivity.this, "Entrez votre nom", Toast.LENGTH_LONG).show();
     }
